753b7bac9c2cd5e43101dedeed1865eb0684fcd0
Nikita <avdeevn1142@gmail.com>
2024-04-17 17:10:50 +0300

    add cart CRUD operation

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,6 @@
+from main.models import Profile
+
+
 class Cart:
     def __init__(self, request):
         self.session = request.session
@@ -20,6 +23,46 @@ class Cart:
 
         self.cart[product_id]['quantity'] += quantity
         self.save()
+        current_user = Profile.objects.filter(user__id=self.request.user.id)
+        # Convert {'3':1, '2':4} to {"3":1, "2":4}
+        carty = str(self.cart)
+        carty = carty.replace("\'", "\"")
+        # Save carty to the Profile Model
+        current_user.update(cart=str(carty))
 
     def save(self):
         self.session.modified = True
+
+    def delete(self, product):
+        product_id = str(product)
+        # Delete from dictionary/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+
+        self.session.modified = True
+
+        # Get the current user profile
+        current_user = Profile.objects.filter(user__id=self.request.user.id)
+        # Convert {'3':1, '2':4} to {"3":1, "2":4}
+        carty = str(self.cart)
+        carty = carty.replace("\'", "\"")
+        # Save carty to the Profile Model
+        current_user.update(cart=str(carty))
+
+
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        # Get cart
+        ourcart = self.cart
+        # Update Dictionary/cart
+        cart = ourcart[product_id]
+        cart['quantity'] = product_qty
+        self.session.modified = True
+        # Get the current user profile
+        current_user = Profile.objects.filter(user__id=self.request.user.id)
+        # Convert {'3':1, '2':4} to {"3":1, "2":4}
+        carty = str(self.cart)
+        carty = carty.replace("\'", "\"")
+        # Save carty to the Profile Model
+        current_user.update(cart=str(carty))
--- cart/views.py
+++ cart/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 from cart.cart import Cart
 from main.models import Product
 
 
 # Create your views here.
 def cart(request):
-    return None
+    cart = Cart(request)
+    products = []
+    total_cost = 0
+    for id, item in cart.cart.items():
+        product = Product.objects.get(id=id)
+        products.append({
+            'product': product,
+            'quantity': item['quantity'],
+            'total_price': product.cost * item['quantity'],
+        })
+        total_cost += product.cost * item['quantity']
+    return render(request, 'cart/cart.html', {'cart': products, 'total_cost': total_cost})
 
 
 def cart_add(request):
@@ -19,5 +31,33 @@ def cart_add(request):
         cart.add_product(product)
 
         return JsonResponse({'qty': sum(item['quantity'] for item in cart.cart.values())})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+def cart_update(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('quantity'))
+
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty': product_qty})
+        return response
+
+
+def cart_delete(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+
+        cart.delete(product=product_id)
+
+        messages.success(request, ("Item Deleted From Shopping Cart"))
+
+        response = JsonResponse({'product': product_id})
+        return response
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
