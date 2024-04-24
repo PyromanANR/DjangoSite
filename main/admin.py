f805b9673047fee5eb58ebfc554585a50d84f935
Nikita <avdeevn1142@gmail.com>
2024-04-25 00:17:46 +0300

    add ProductAdmin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Category, Product, Profile
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('category',)
+
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Profile)
